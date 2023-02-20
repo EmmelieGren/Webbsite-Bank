a14020f4f222dc452327333052278b516ba6a279
Emmelie Gren <emmelierod@gmail.com>
2023-02-20 12:00:12 +0100

    test.py

--- forms.py
+++ forms.py
@@ -3,18 +3,14 @@ from wtforms import Form, BooleanField, StringField, PasswordField, validators, 
 from wtforms.fields import IntegerField, SelectField, DateField, DecimalField, EmailField
 from datetime import datetime
 from datetime import timedelta  
-import re
+# from Test import FormsTestCases
 
 #if not field.data.endswith('.[AZ]|[az]{2,3}'):
 
-def emailContains(form, field):
-    if not field.data.endswith('.se'):
-        raise ValidationError('Have to end with ex: .se')
-
 # def emailContains(form, field):
-#     valid = r'\b[A-Za-z]+@[A-Za-z]+\.[A-Z|a-z]{2,7}\b'
-#     if not field.data(re.fullmatch(valid)):
-#         raise ValidationError('Invalid Email')
+#     if not field.data.endswith('.se'):
+#         raise ValidationError('Have to end with ex: .se')
+
 
 
 def stringContains(form, field):
@@ -30,10 +26,10 @@ class NewCustomerForm(FlaskForm):
     country = StringField('country', validators=[validators.DataRequired()])
     countryCode = StringField('countrycode', validators=[validators.DataRequired()])
     birthday = DateField('birthday', validators=[validators.DataRequired()])                 
-    nationalId = IntegerField('nationalId', validators=[validators.DataRequired()])
+    nationalId = StringField('nationalId', validators=[validators.DataRequired()])
     telephoneCountryCode = IntegerField('telephoneCountryCode', validators=[validators.DataRequired()])
     telephone = StringField('telephone', validators=[validators.DataRequired()])
-    emailAddress = EmailField('emailadress', validators=[validators.DataRequired(),emailContains])
+    emailAddress = EmailField('emailadress', validators=[validators.DataRequired()])
 
 class NewAccountForm(FlaskForm):
     AccountType = SelectField('AccountType',choices=[('Personal', 'Personal'),('Checking', 'Checking'),('Savings', 'Savings')])
@@ -44,6 +40,9 @@ class TransactionForm(FlaskForm):
 
 class TransferForm(FlaskForm):
     Amount = DecimalField('Amount', validators=[validators.DataRequired(), validators.NumberRange(min=1,max=5000)])
-    Id = IntegerField('nationalId', validators=[validators.DataRequired()])
+    Id = IntegerField('Id', validators=[validators.DataRequired()])
 
 
+class LostPw(FlaskForm):
+    emailAddress = EmailField('emailadress', validators=[validators.DataRequired()])
+    pw = StringField('password', validators=[validators.DataRequired()])
