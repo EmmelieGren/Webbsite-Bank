d4630c1a71e66bc50a127a76bb7628dcf709421a
Emmelie Gren <emmelierod@gmail.com>
2023-03-05 14:47:07 +0100

    validering

--- forms.py
+++ forms.py
@@ -2,15 +2,6 @@ from flask_wtf import FlaskForm
 from wtforms import Form, BooleanField, StringField, PasswordField, validators, ValidationError
 from wtforms.fields import IntegerField, SelectField, DateField, DecimalField, EmailField
 
-def emailContains(form, field):
-    if not field.data.endswith('.se'):
-        raise ValidationError('Have to end with ex: .se')
-
-
-def stringContains(form, field):
-    if field.data() == IntegerField:
-        raise ValidationError('Can not be Numbers')
-
 class NewCustomerForm(FlaskForm):
     givenName = StringField('givenname', validators=[validators.DataRequired(), validators.Length(max= 50)])
     surname = StringField('surname', validators=[validators.DataRequired(), validators.Length(max= 50)])
@@ -23,7 +14,7 @@ class NewCustomerForm(FlaskForm):
     nationalId = StringField('telephoneCountryCode', validators=[validators.DataRequired()])
     telephoneCountryCode = SelectField('AccountType',choices=[('46', '46'),('47', '47'),('55', '55')])
     telephone = StringField('telephone', validators=[validators.DataRequired()])
-    emailAddress = EmailField('emailadress', validators=[validators.DataRequired(),emailContains])
+    emailAddress = EmailField('emailadress', validators=[validators.DataRequired()])
 
 class NewAccountForm(FlaskForm):
     AccountType = SelectField('AccountType',choices=[('Personal', 'Personal'),('Checking', 'Checking'),('Savings', 'Savings')])
